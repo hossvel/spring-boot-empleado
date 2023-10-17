63571a7ed3658eb0499248393a59676c7c6d014c
devhoss <velasco2288@gmail.com>
2023-10-17 15:00:51 -0500

    pruebas uniarias controller para findbydni

--- src/test/java/com/devhoss/repository/IEmpleadoRepositoryTests.java
+++ src/test/java/com/devhoss/repository/IEmpleadoRepositoryTests.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -67,4 +68,47 @@ class IEmpleadoRepositoryTests {
         assertThat(empleadoBD).isNotNull();
     }
 
+
+    @DisplayName("Test para actualizar un empleado")
+    @Test
+    void testActualizarEmpleado(){
+        iempleadoRepository.save(empleado);
+
+        //when
+        Empleado empleadoGuardado = iempleadoRepository.findById(empleado.getId()).get();
+        empleadoGuardado.setEmail("hhh-modificado@gmail.com");
+        empleadoGuardado.setNombre("Hoss Hoss");
+        empleadoGuardado.setApellido("VelascoA");
+        Empleado empleadoActualizado = iempleadoRepository.save(empleadoGuardado);
+
+        //then
+        assertThat(empleadoActualizado.getEmail()).isEqualTo("hhh-modificado@gmail.com");
+        assertThat(empleadoActualizado.getNombre()).isEqualTo("Hoss Hoss");
+    }
+
+    @DisplayName("Test para eliminar un empleado")
+    @Test
+    void testEliminarEmpleado(){
+        iempleadoRepository.save(empleado);
+
+        //when
+        iempleadoRepository.deleteById(empleado.getId());
+        Optional<Empleado> empleadoOptional = iempleadoRepository.findById(empleado.getId());
+
+        //then
+        assertThat(empleadoOptional).isEmpty();
+    }
+
+    @DisplayName("Test para obtener un empleado por ID")
+    @Test
+    void testObtenerEmpleadoPorDni(){
+        iempleadoRepository.save(empleado);
+
+        //when - comportamiento o accion que vamos a probar
+        Empleado empleadoBD = iempleadoRepository.findByDni(empleado.getDni()).get();
+
+        //then
+        assertThat(empleadoBD).isNotNull();
+        assertEquals(empleadoBD.getDni(),"12345678");
+    }
 }
